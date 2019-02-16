97a553dca8fbb716059449c6344c96e51241cb16
Harsh Agrawal <harsh.agrawal17@siesgst.ac.in>
2019-02-16 23:26:49 +0530

    Second commit with mistakes

--- app/src/main/java/com/example/harsh/cktdesigning/MainActivity.java
+++ app/src/main/java/com/example/harsh/cktdesigning/MainActivity.java
@@ -1,7 +1,10 @@
 package com.example.harsh.cktdesigning;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +12,15 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Button SingleStage = (Button) findViewById(R.id.SingleStage);
+
+        SingleStage.setOnClickListener(new View.OnClickListener() {
+        @Override
+        public void onClick(View view) {
+            Intent numbersIntent = new Intent(MainActivity.this, SingleStage.class);
+            startActivity(numbersIntent);
+            }
+        });
+
     }
 }
---
+++ app/src/main/java/com/example/harsh/cktdesigning/SingleStage.java
@@ -0,0 +1,13 @@
+package com.example.harsh.cktdesigning;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class SingleStage extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_single_stage);
+    }
+}
