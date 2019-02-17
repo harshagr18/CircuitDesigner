cb52beff9b797c78a897cd7621bd8491dfb58a19
Harsh Agrawal <harsh.agrawal17@siesgst.ac.in>
2019-02-17 12:23:33 +0530

    Added ce-cs views and working on ce frontend

--- app/src/main/java/com/example/harsh/cktdesigning/SingleStage.java
+++ app/src/main/java/com/example/harsh/cktdesigning/SingleStage.java
@@ -1,7 +1,10 @@
 package com.example.harsh.cktdesigning;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class SingleStage extends AppCompatActivity {
 
@@ -9,5 +12,26 @@ public class SingleStage extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_single_stage);
+
+
+        Button CsAmp = (Button) findViewById(R.id.CsAmp);
+        CsAmp.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent Cslaunch = new Intent(SingleStage.this, csamplifier.class);
+                startActivity(Cslaunch);
+            }
+        });
+
+
+        Button CeAmp = (Button) findViewById(R.id.CeAmp);
+        CeAmp.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent Celaunch = new Intent(SingleStage.this, ceamplifier.class);
+                startActivity(Celaunch);
+            }
+        });
+
     }
 }
---
+++ app/src/main/java/com/example/harsh/cktdesigning/ceamplifier.java
@@ -0,0 +1,13 @@
+package com.example.harsh.cktdesigning;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ceamplifier extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_ceamplifier);
+    }
+}
